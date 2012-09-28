3a27b60319e3e30faf0774ce2b76075a8d92b9fa
jonasmalacofilho@gmail.com <jonas.malaco>
2012-09-28 15:18:52 +0000

    minor doc

--- src/jonas/Timezone.hx
+++ src/jonas/Timezone.hx
@@ -5,12 +5,14 @@ package jonas;
    Licensed under the MIT license. Check LICENSE.txt for more information. */
 class Timezone {
 	
+	// Returns the local (invariant on time) timezone in miliseconds
 	public static function localTimezone() : Float {
 		var x1 = 181. * 24 * 3600 * 1000 - new Date( 1970, 6, 1, 0, 0, 0 ).getTime();
 		var x2 = 365. * 24 * 3600 * 1000 - new Date( 1971, 0, 1, 0, 0, 0 ).getTime();
 		return Math.min( x1, x2 );
 	}
 
+	// Returns the current (with/without daylight savings time) timezone in miliseconds
 	public static function currentTimezone( ?date=null ) : Float {
 		if ( date==null )
 			date = Date.now();
