a101dc1149fc723dd8b3a1cf7c31e189ff4b09b0
jonasmalacofilho@gmail.com <jonas.malaco>
2012-10-18 16:49:26 +0000

    RTree interface and in-memory SQLiteRTree (requires RTREE support on the SQLite library/slower than RjTree)

---
+++ src/jonas/ds/SQLiteRTree.hx
@@ -0,0 +1,90 @@
+package jonas.ds;
+
+using jonas.LazyLambda;
+
+// compilter was getting lost with empty: Void -> Bool and empty: jonas.Maybe<A>
+private enum Maybe<T> {
+	Empty;
+	Just( v: T );
+}
+
+class SQLiteRTree<T> implements RTree<T> {
+	
+	public var length( default, null ): Int;
+	
+	var cnx: sys.db.Connection;
+	var objs: Array<Maybe<T>>;
+
+	public function new() {
+		cnx = sys.db.Sqlite.open( ':memory:' );
+		cnx.request( 'CREATE VIRTUAL TABLE tree USING rtree ( id, minX, maxX, minY, maxY )' );
+		cnx.request( 'PRAGMA journal_mode=OFF' );
+		cnx.request( 'PRAGMA synchronous=OFF' );
+		objs = [];
+		length = 0;
+	}
+
+	public function search( xMin: Float, yMin: Float, width: Float, height: Float ): Iterator<T> {
+		var res = cnx.request( Std.format( 'SELECT id FROM tree WHERE minX<=${width+xMin} AND minY<=${height+yMin} AND maxX>=${xMin} AND maxY>=${yMin}' ) ).lazy();
+		return res.map( switch ( objs[$x.id] ) { case Just( x ): x; default: throw $x.id; } ).iterator();
+	}
+
+	public function iterator(): Iterator<T> {
+		return objs.map( switch ( $x ) { case Just( x ): x; default: throw $x; } ).iterator();
+	}
+
+	public function insertPoint( x: Float, y: Float, object: T ): Void {
+		var curId = objs.length;
+		objs[curId] = Just( object );
+		cnx.request( Std.format( 'INSERT INTO tree ( id, minX, maxX, minY, maxY ) VALUES ( $curId, $x, $x, $y, $y )' ) );
+		length++;
+		jonas.macro.Error.throwIf( cnx.request( 'SELECT count( 1 ) cnt FROM tree' ).next().cnt != length );
+	}
+
+	public function insertRectangle( x: Float, y: Float, width: Float, height: Float, object: T ) : Void {
+		var curId = objs.length;
+		objs[curId] = Just( object );
+		cnx.request( Std.format( 'INSERT INTO tree ( id, minX, maxX, minY, maxY ) VALUES ( $curId, $x, ${width+x}, ${y}, ${height+y} )' ) );
+		length++;
+		jonas.macro.Error.throwIf( cnx.request( 'SELECT count( 1 ) cnt FROM tree' ).next().cnt != length );
+	}
+
+	public function removePoint( x: Float, y: Float, ?object: Null<T> ): Int {
+		var res = cnx.request( Std.format( 'SELECT id FROM tree WHERE minX<=${x} AND minY<=${y} AND maxX>=${x} AND maxY>=${y}' ) ).lazy();
+		var cnt = 0;
+		for ( id in res.map( $x.id ) )
+			if ( object == null || Type.enumEq( objs[id], Just( object ) ) ) {
+				objs[id] = Empty;
+				cnx.request( Std.format( 'DELETE FROM tree WHERE id=$id' ) );
+				length--;
+				cnt++;
+			}
+		return cnt;
+	}
+
+	public function removeRectangle( x: Float, y: Float, width: Float, height: Float, ?object: T ): Int {
+		var res = cnx.request( Std.format( 'SELECT id FROM tree WHERE minX<=${width+x} AND minY<=${height+y} AND maxX>=${x} AND maxY>=${y}' ) ).lazy();
+		var cnt = 0;
+		for ( id in res.map( $x.id ) )
+			if ( object == null || Type.enumEq( objs[id], Just( object ) ) ) {
+				objs[id] = Empty;
+				cnx.request( Std.format( 'DELETE FROM tree WHERE id=$id' ) );
+				length--;
+				cnt++;
+			}
+		return cnt;
+	}
+
+	public function removeObject( object: T ): Int {
+		var cnt = 0;
+		for ( id in 0...objs.length )
+			if ( Type.enumEq( objs[id], Just( object ) ) ) {
+				objs[id] = Empty;
+				cnx.request( Std.format( 'DELETE FROM tree WHERE id=$id' ) );
+				length--;
+				cnt++;
+			}
+		return cnt;
+	}
+
+}
